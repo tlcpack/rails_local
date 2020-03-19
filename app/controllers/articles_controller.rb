19a7aefb1c7a4155bbec9af62ee8b543db438c9e
Taylor Cooke <taylorcooke@gmail.com>
2020-03-18 20:56:50 -0400

    added flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,9 @@ class ArticlesController < ApplicationController
     def create
         @article = Article.new(article_params)
         @article.save
+
+        flash.notice = "Article '#{@article.title}' created!"
+
         redirect_to article_path(@article)
     end
 
@@ -32,6 +35,8 @@ class ArticlesController < ApplicationController
     def update
         @article = Article.find(params[:id])
         @article.update(article_params)
+
+        flash.notice = "Article '#{@article.title}' updated!'"
       
         redirect_to article_path(@article)
       end
